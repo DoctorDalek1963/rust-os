702a2a554b423dc78aced1a51e1710886e65a44a
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-09-21 23:59:55 +0100

    Add a simple VGA buffer abstraction layer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+//! `rust_os` is a kernel written in Rust based on Philipp Oppermann's blog series.
+
 #![no_std]
 #![no_main]
+
+mod vga_buffer;
 
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
@@ -11,14 +15,22 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    use self::vga_buffer::{Color, ColorCode, Writer};
+    use core::fmt::Write;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    let mut writer = Writer::new(ColorCode::new(Color::White, Color::Black));
+
+    write!(writer, r"The numbers are {} and {}
+
+This is some more text. Some placeholder text, if you will. The idea here is to have a really long line that forces some line wrapping.
+
+And then
+some
+manual
+newlines
+
+And some non-ASCII: Ελλενικα
+    ", 42, 1.0 / 3.0).unwrap();
 
     #[allow(clippy::empty_loop)]
     loop {}
